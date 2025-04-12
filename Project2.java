b5181fc012a4c57439d74aca27489ec31b15a900
Azaduddin2001 <mdazaduddin2001@gmail.com>
2025-04-12 15:09:36 +0600

    Project2

--- Project2.java
+++ Project2.java
@@ -1,5 +1,6 @@
 public class Project2{
     public static void main(String[] args) {
         System.out.println("hi How are you");
+        System.out.println("I am good,Thank you for asking");
     }
 }
