5e4a9dcb00847eb273a809d6522bb185b27fb9ca
Azaduddin2001 <mdazaduddin2001@gmail.com>
2025-04-12 18:55:36 +0600

    Project3

--- Project2.java
+++ Project2.java
@@ -2,5 +2,6 @@ public class Project2{
     public static void main(String[] args) {
         System.out.println("hi How are you");
         System.out.println("I am good,Thank you for asking");
+        System.out.println("i am form Bangladesh ,i live in Rajshahi");
     }
 }
